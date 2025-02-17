1a9a83ff757c07794a38f2f5b899ecc2da0ae7d5
glitch452 <894128+glitch452@users.noreply.github.com>
2025-02-17 15:14:56 -0800

    test: Update Vitest to v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 const baseConfig = defineConfig({
   test: {
     globals: true,
+    reporters: ['verbose'],
     coverage: {
       reporter: ['text'],
       include: ['src/**/*.ts'],
