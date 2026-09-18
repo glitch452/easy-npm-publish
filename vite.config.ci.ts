bd0e368c0c82825b1e647aa1dd26300370a65edd
glitch452 <894128+glitch452@users.noreply.github.com>
2026-09-18 10:37:15 -0700

    chore: Fix no-generated-empty-object-type lint error in vite.config.ci

--- vite.config.ci.ts
+++ vite.config.ci.ts
@@ -2,12 +2,13 @@ import { defineConfig } from 'vitest/config';
 import { type InlineConfig } from 'vitest/node';
 import baseConfig from './vite.config.js';
 
-const baseVitestConfig = baseConfig.test;
-const baseCoverage: Omit<InlineConfig['coverage'], 'reporter'> | undefined = baseVitestConfig?.coverage;
+const { test: baseConfigTest, ...baseConfigRest } = baseConfig;
+const baseCoverage: Omit<NonNullable<InlineConfig['coverage']>, 'reporter'> | undefined = baseConfigTest?.coverage;
 
 const configForCi = defineConfig({
+  ...baseConfigRest,
   test: {
-    ...baseVitestConfig,
+    ...baseConfigTest,
     coverage: {
       ...baseCoverage,
       reporter: ['text', 'json', 'json-summary'],
